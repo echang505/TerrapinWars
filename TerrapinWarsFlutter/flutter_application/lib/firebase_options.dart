556e3ebe5c9098ef510265afed636821e6254bb9
Eric Chang <82834722+echang505@users.noreply.github.com>
2024-04-29 19:00:19 -0400

    Update firebase_options.dart

--- TerrapinWarsFlutter/flutter_application/lib/firebase_options.dart
+++ TerrapinWarsFlutter/flutter_application/lib/firebase_options.dart
@@ -45,49 +45,22 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyC2AQ_6omYbaZ0xq3Rq4L6OygR07cd1ouY',
-    appId: '1:29479499308:web:0370dd7af1e8b332a58389',
-    messagingSenderId: '29479499308',
-    projectId: 'algebraic-argon-420916',
-    authDomain: 'algebraic-argon-420916.firebaseapp.com',
-    databaseURL: 'https://algebraic-argon-420916-default-rtdb.firebaseio.com',
-    storageBucket: 'algebraic-argon-420916.appspot.com',
-    measurementId: 'G-1XYCCV5LW2',
+    ###
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBW4fGwDtcPGVuW3h7Bjp6WtNw89xQ5VVo',
-    appId: '1:29479499308:android:7ae3eeb7af86ee4ca58389',
-    messagingSenderId: '29479499308',
-    projectId: 'algebraic-argon-420916',
-    storageBucket: 'algebraic-argon-420916.appspot.com',
+   ###
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDh8VpN6tm2XkAasgvYQo2UazWNG3jdjTQ',
-    appId: '1:29479499308:ios:fd69bc819ec4fdfaa58389',
-    messagingSenderId: '29479499308',
-    projectId: 'algebraic-argon-420916',
-    storageBucket: 'algebraic-argon-420916.appspot.com',
-    iosBundleId: 'com.example.flutterApplication',
+   ###
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDh8VpN6tm2XkAasgvYQo2UazWNG3jdjTQ',
-    appId: '1:29479499308:ios:fd69bc819ec4fdfaa58389',
-    messagingSenderId: '29479499308',
-    projectId: 'algebraic-argon-420916',
-    storageBucket: 'algebraic-argon-420916.appspot.com',
-    iosBundleId: 'com.example.flutterApplication',
+    ###
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyC2AQ_6omYbaZ0xq3Rq4L6OygR07cd1ouY',
-    appId: '1:29479499308:web:f0703207a6917077a58389',
-    messagingSenderId: '29479499308',
-    projectId: 'algebraic-argon-420916',
-    authDomain: 'algebraic-argon-420916.firebaseapp.com',
-    storageBucket: 'algebraic-argon-420916.appspot.com',
-    measurementId: 'G-S7TCHN5R7C',
+   ###
   );
 }
